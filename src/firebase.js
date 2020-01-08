87629c78cc306d9dc8a01d335a60cfe159b3a923
jakemccauley97 <59584904+jakemccauley97@users.noreply.github.com>
2020-01-08 16:17:43 -0700

    changed

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCCJHEkjmDsGwiZCL8dfft_qwZHQB91itg",
+  authDomain: "todo-58494.firebaseapp.com",
+  databaseURL: "https://todo-58494.firebaseio.com",
+  projectId: "todo-58494",
+  storageBucket: "todo-58494.appspot.com",
+  messagingSenderId: "487915053328",
+  appId: "1:487915053328:web:c12b45d9410eae5e4c8595"
 };
 
 firebase.initializeApp(config);
